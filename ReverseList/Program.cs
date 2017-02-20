c74fd10eb2fed841dc3bdc5442f1cec0aaf205e3
OlayG <ogureje@yahoo.com>
2017-02-20 11:22:48 -0800

    Reverse list without using built in .Reverse() property

--- ReverseList/Program.cs
+++ ReverseList/Program.cs
@@ -10,6 +10,28 @@ namespace ReverseList
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int>();
+            int len = 100;
+            PopulateList(numbers, len);
+            ReverseTheList(numbers);
+
+            Console.ReadKey();
+        }
+
+        private static void ReverseTheList(List<int> numbers)
+        {
+            for(int i = numbers.Count -1; i >= 0; i--)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+        }
+
+        private static void PopulateList(List<int> numbers, int len)
+        {
+            for (int i = 0; i < len; i++)
+            {
+                numbers.Add(i + 1);
+            }
         }
     }
 }
